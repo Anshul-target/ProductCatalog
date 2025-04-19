f288878d05de6780d5ea6f8b3c9f517b1436c2ae
Anshul <ayushyadav6a@gmail.com>
2025-04-19 11:56:04 +0530

    Initialised the app

--- src/main/java/com/ecom/ProductCatalogService/controller/AdminController.java
+++ src/main/java/com/ecom/ProductCatalogService/controller/AdminController.java
@@ -24,7 +24,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Controller
+@RestController
 @RequestMapping("/api/admin")
 public class AdminController {
 //CategoryService
